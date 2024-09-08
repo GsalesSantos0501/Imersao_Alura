95b61bbabee91b40ccd83b3fcd834e5bdab47328
GsalesSantos0501 <gasantista51@gmail.com>
2024-09-08 17:09:30 -0300

    commit

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, também conhecida como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma.Aos 13 anos, tornou se a mais jovem medalhista olímpica da história do Brasil.Sua trajetória inspiradora e seu estilo único a tornaram um ícone do esporte.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nas competições nacionais e internacionais. Sua técnica apurada e determinação a tornaram uma das principais atletas do judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com sua força e elegância. Ela é a primeira ginasta brasileira a conquistar uma medalha de ouro olímpica.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    }
+];
